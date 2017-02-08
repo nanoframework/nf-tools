92ce5d5f450cfbcc7b23ac0d6d0d7fedcc6d8eaa
CW2 <cw2@users.noreply.github.com>
2017-02-08 23:52:48 +0100

    Initial import

---
+++ GitHubDcoCheck/Extensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿//
+// Copyright (c) 2017 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+namespace GitHubDcoCheck
+{
+    using System;
+
+    internal static class StringExtensions
+    {
+        public static string FormatWith(this string format, params object[] args)
+        {
+            return String.Format(format, args);
+        }
+    }
+}
