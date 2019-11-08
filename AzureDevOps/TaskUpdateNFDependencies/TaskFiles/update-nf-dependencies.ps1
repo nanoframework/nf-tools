6a76f7c1f806cb3c2283ffe0619afd1c687cf1bd
José Simões <jose.simoes@eclo.solutions>
2019-11-08 16:20:21 +0000

    Improvements on udpate dependents Azure task
    - Now the PR is only started if there is anything to update.
    - On PR error an exception is thrown so the build fails.
    - Bump version to 1.1.26.
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- AzureDevOps/TaskUpdateNFDependencies/TaskFiles/update-nf-dependencies.ps1
+++ AzureDevOps/TaskUpdateNFDependencies/TaskFiles/update-nf-dependencies.ps1
@@ -276,79 +276,80 @@ ForEach($library in $librariesToUpdate)
         # something went wrong as no package was updated and it should be at least one
         'No packages were updated...' | Write-Host -ForegroundColor Yellow
     }
-
-    # need this line so nfbot flags the PR appropriately
-    $commitMessage += "`n[version update]`n`n"
-
-    # better add this warning line               
-    $commitMessage += "### :warning: This is an automated update. Merge only after all tests pass. :warning:`n"
-
-    
-    Write-Debug "Git branch" 
-
-    # create branch to perform updates
-    git branch $newBranchName
-
-    Write-Debug "Checkout branch" 
-    
-    # checkout branch
-    git checkout $newBranchName
-
-    Write-Debug "Add changes" 
-    
-    # commit changes
-    git add -A > $null
-
-    # commit message with a different title if one or more dependencies are updated
-    if ($updateCount -gt 1)
+    else
     {
-        Write-Debug "Commit changed file" 
+        # need this line so nfbot flags the PR appropriately
+        $commitMessage += "`n[version update]`n`n"
 
-        git commit -m "Update $updateCount NuGet dependencies ***NO_CI***" -m"$commitMessage" > $null
+        # better add this warning line               
+        $commitMessage += "### :warning: This is an automated update. Merge only after all tests pass. :warning:`n"
 
-        # fix PR title
-        $prTitle = "Update $updateCount NuGet dependencies"
+        
+        Write-Debug "Git branch" 
+
+        # create branch to perform updates
+        git branch $newBranchName
+
+        Write-Debug "Checkout branch" 
+        
+        # checkout branch
+        git checkout $newBranchName
+
+        Write-Debug "Add changes" 
+        
+        # commit changes
+        git add -A > $null
+
+        # commit message with a different title if one or more dependencies are updated
+        if ($updateCount -gt 1)
+        {
+            Write-Debug "Commit changed file" 
+
+            git commit -m "Update $updateCount NuGet dependencies ***NO_CI***" -m"$commitMessage" > $null
+
+            # fix PR title
+            $prTitle = "Update $updateCount NuGet dependencies"
+        }
+        else 
+        {
+            Write-Debug "Commit changed files"
+
+            git commit -m "$prTitle ***NO_CI***" -m "$commitMessage" > $null
+        }
+
+        Write-Debug "Push changes"
+
+        git -c http.extraheader="AUTHORIZATION: $auth" push --set-upstream origin $newBranchName > $null
+
+        # start PR
+        # we are hardcoding to 'develop' branch to have a fixed one
+        # this is very important for tags (which don't have branch information)
+        # considering that the base branch can be changed at the PR ther is no big deal about this 
+        $prRequestBody = @{title="$prTitle";body="$commitMessage";head="$newBranchName";base="develop"} | ConvertTo-Json
+        $githubApiEndpoint = "https://api.github.com/repos/nanoframework/$library/pulls"
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+        $headers = @{}
+        $headers.Add("Authorization","$auth")
+        $headers.Add("Accept","application/vnd.github.symmetra-preview+json")
+
+        try 
+        {
+            $result = Invoke-RestMethod -Method Post -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer -Uri  $githubApiEndpoint -Header $headers -ContentType "application/json" -Body $prRequestBody
+            'Started PR with dependencies update...' | Write-Host -NoNewline
+            'OK' | Write-Host -ForegroundColor Green
+        }
+        catch 
+        {
+            $result = $_.Exception.Response.GetResponseStream()
+            $reader = New-Object System.IO.StreamReader($result)
+            $reader.BaseStream.Position = 0
+            $reader.DiscardBufferedData()
+            $responseBody = $reader.ReadToEnd();
+
+            throw "Error starting PR: $responseBody"
+        }
     }
-    else 
-    {
-        Write-Debug "Commit changed files"
-
-        git commit -m "$prTitle ***NO_CI***" -m "$commitMessage" > $null
-    }
-
-    Write-Debug "Push changes"
-
-    git -c http.extraheader="AUTHORIZATION: $auth" push --set-upstream origin $newBranchName > $null
-
-    # start PR
-    # we are hardcoding to 'develop' branch to have a fixed one
-    # this is very important for tags (which don't have branch information)
-    # considering that the base branch can be changed at the PR ther is no big deal about this 
-    $prRequestBody = @{title="$prTitle";body="$commitMessage";head="$newBranchName";base="develop"} | ConvertTo-Json
-    $githubApiEndpoint = "https://api.github.com/repos/nanoframework/$library/pulls"
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-    $headers = @{}
-    $headers.Add("Authorization","$auth")
-    $headers.Add("Accept","application/vnd.github.symmetra-preview+json")
-
-    try 
-    {
-        $result = Invoke-RestMethod -Method Post -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer -Uri  $githubApiEndpoint -Header $headers -ContentType "application/json" -Body $prRequestBody
-        'Started PR with dependencies update...' | Write-Host -NoNewline
-        'OK' | Write-Host -ForegroundColor Green
-    }
-    catch 
-    {
-        $result = $_.Exception.Response.GetResponseStream()
-        $reader = New-Object System.IO.StreamReader($result)
-        $reader.BaseStream.Position = 0
-        $reader.DiscardBufferedData()
-        $responseBody = $reader.ReadToEnd();
-
-        "Error starting PR: $responseBody" | Write-Host -ForegroundColor Red
-    }
-
 }
 
 Trace-VstsLeavingInvocation $MyInvocation
