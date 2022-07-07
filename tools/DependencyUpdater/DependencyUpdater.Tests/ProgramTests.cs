e54a742aafa16b28a9d54c9189b5f644fc93cbe9
torbacz <nanoframework@outlook.com>
2022-07-07 16:17:38 +0200

    Migrate to MSTest from xunit

--- tools/DependencyUpdater/DependencyUpdater.Tests/ProgramTests.cs
+++ tools/DependencyUpdater/DependencyUpdater.Tests/ProgramTests.cs
@@ -5,27 +5,28 @@
 
 namespace DependencyUpdater.Tests;
 
+[TestClass]
 public class ProgramTests
 {
-    [Theory]
-    [InlineData("origin	https://github.com/torbacz/nf-tools.git (fetch) origin	https://github.com/torbacz/nf-tools.git (push)", "nf-tools")]
-    [InlineData("origin	https://github.com/nanoframework/nf-tools.git (fetch) origin	https://github.com/nanoframework/nf-tools.git (push)", "nf-tools")]
+    [DataTestMethod]
+    [DataRow("origin	https://github.com/torbacz/nf-tools.git (fetch) origin	https://github.com/torbacz/nf-tools.git (push)", "nf-tools")]
+    [DataRow("origin	https://github.com/nanoframework/nf-tools.git (fetch) origin	https://github.com/nanoframework/nf-tools.git (push)", "nf-tools")]
     public void GetLibNameFromGitString_Should_ReturnValidData(string inputData, string expectedResult)
     {
         var result = nanoFramework.Tools.DependencyUpdater.Program.GetRepoNameFromInputString(inputData);
-        Assert.True(result.Success);
+        Assert.IsTrue(result.Success);
         
         var libName = nanoFramework.Tools.DependencyUpdater.Program.GetLibNameFromRegexMatch(result);
-        Assert.Equal(expectedResult, libName);
+        Assert.AreEqual(expectedResult, libName);
     }
 
-    [Theory]
-    [InlineData("origin	https://github.com/torbacz/nf-tools.git (fetch) origin	https://github.com/torbacz/nf-tools.git (push)", "torbacz")]
-    [InlineData("origin	https://github.com/nanoframework/nf-tools.git (fetch) origin	https://github.com/nanoframework/nf-tools.git (push)", "nanoframework")]
-    [InlineData("origin	https://github.com/nanoframework-test/nf-tools.git (fetch) origin	https://github.com/nanoframework/nf-tools.git (push)", "nanoframework-test")]
+    [DataTestMethod]
+    [DataRow("origin	https://github.com/torbacz/nf-tools.git (fetch) origin	https://github.com/torbacz/nf-tools.git (push)", "torbacz")]
+    [DataRow("origin	https://github.com/nanoframework/nf-tools.git (fetch) origin	https://github.com/nanoframework/nf-tools.git (push)", "nanoframework")]
+    [DataRow("origin	https://github.com/nanoframework-test/nf-tools.git (fetch) origin	https://github.com/nanoframework/nf-tools.git (push)", "nanoframework-test")]
     public void GetRepoOwnerFromInputString_Should_ReturnValidData(string inputData, string expectedResult)
     {
         var result = nanoFramework.Tools.DependencyUpdater.Program.GetRepoOwnerFromInputString(inputData);
-        Assert.Equal(expectedResult, result);
+        Assert.AreEqual(expectedResult, result);
     }
 }
