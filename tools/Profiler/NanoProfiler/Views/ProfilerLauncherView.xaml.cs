d9bde7e53a226313ce2b5c95cee3db268b85eff9
Mike Muegel <mike.muegel@skyworksinc.com>
2023-09-18 09:08:06 -0500

    Profiler: improve GUI update performance
    Achieved through update of logger text control periodically in thread and limit the # of messages displayed

--- tools/Profiler/NanoProfiler/Views/ProfilerLauncherView.xaml.cs
+++ tools/Profiler/NanoProfiler/Views/ProfilerLauncherView.xaml.cs
@@ -5,6 +5,10 @@
 
 using CommunityToolkit.Mvvm.Messaging;
 using nanoFramework.Tools.NanoProfiler.ViewModels;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Threading;
@@ -19,28 +23,89 @@ namespace nanoFramework.Tools.NanoProfiler.Views
         private ICommand ViewUnLoaded { get; set; }
         private ProfilerLauncherViewModel _viewModel;
 
+        // Trace logging (in GUI) related
+        private const int maxLogMessages = 1000;
+        private Queue<string> logMessages = new Queue<string>();
+        private bool logTruncated = false;
+        private System.Timers.Timer logGUIUpdateTimer;
+        private Guid logMessagesFingerprint = new Guid();
+        private Guid lastDisplayedLogMessagesFingerprint = new Guid();
+
         public ProfilerLauncherView()
         {
             InitializeComponent();
 
+            // A timer is used to update the on-screen trace logger every 500msec: otherwise
+            // impossible to keep up
+            logGUIUpdateTimer = new System.Timers.Timer(500);
+            logGUIUpdateTimer.AutoReset = true;
+            logGUIUpdateTimer.Elapsed += LogGUIUpdateTimer_Elapsed;
+            logGUIUpdateTimer.Start();
+
+            clearLog();
+
             // register message to update log text
             WeakReferenceMessenger.Default.Register<UpdateLogTextMessage>(this, (r, m) =>
             {
-                Dispatcher.BeginInvoke(DispatcherPriority.Background, () =>
+                lock(this)
                 {
-                    textLog.AppendText(m.Value + "\r\n");
-                    textLog.ScrollToEnd();
-                });
+                    if (logMessages.Count >= maxLogMessages)
+                    {
+                        logTruncated = true;
+                        logMessages.Dequeue();
+                    }
+                    logMessages.Enqueue(m.Value);
+                    logMessagesFingerprint = Guid.NewGuid();
+                }
             });
 
             // register message to clear log text
             WeakReferenceMessenger.Default.Register<ClearLogTextMessage>(this, (r, m) =>
             {
-                Dispatcher.BeginInvoke(DispatcherPriority.Background, () =>
-                {
-                    textLog.Clear();
-                });
+                clearLog();
             });
+
+            Closed += ProfilerLauncherView_Closed;
+        }
+
+        private void ProfilerLauncherView_Closed(object? sender, EventArgs e)
+        {
+            logGUIUpdateTimer.Elapsed -= LogGUIUpdateTimer_Elapsed;
+
+            // TODO: shouldn't something like _viewModel.Cancel() be called here?
+            // Something to immediately stop executing threads and ensure EXE exits fairly quickly
+            // (unlike clicking disconnect button which is very slow)
+        }
+
+        private void LogGUIUpdateTimer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
+        {
+            lock(this)
+            {
+                if (lastDisplayedLogMessagesFingerprint != logMessagesFingerprint)
+                {
+                    lastDisplayedLogMessagesFingerprint = logMessagesFingerprint;
+                    string messages = String.Join(Environment.NewLine, logMessages);
+                    if (logTruncated)
+                    {
+                        messages = $"[ truncated; only last {maxLogMessages} messages shown ]" + Environment.NewLine + messages;
+                    }
+
+                    Dispatcher.BeginInvoke(DispatcherPriority.Background, () =>
+                    {
+                        textLog.Text = messages;
+                        textLog.ScrollToEnd();
+                    });
+                }
+            }
+        }
+
+        private void clearLog()
+        {
+            lock (this)
+            {
+                logTruncated = false;
+                logMessages.Clear();
+            }
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
