8e3e4d16d3853371cdeaab89d7da2d95f77d7b01
josesimoes <jose.simoes@eclo.solutions>
2024-06-12 12:57:53 +0100

    [github-nfbot] Fix sending feedback to comments with commands
    - New ocktokit API is more picky with the parameters.

--- github-nfbot/GitHub-nfbot/GitHub_nfbot.cs
+++ github-nfbot/GitHub-nfbot/GitHub_nfbot.cs
@@ -423,22 +423,22 @@ namespace nanoFramework.Tools.GitHub
                             if (processResult == StartReleaseResult.Executed)
                             {
                                 // add thumbs up reaction to comment
-                                await _octokitClient.Reaction.IssueComment.Create(repositoryId, (int)payload.comment.id, new NewReaction(ReactionType.Plus1));
+                                await _octokitClient.Reaction.IssueComment.Create(repositoryId, (uint)payload.comment.id.Value, new NewReaction(ReactionType.Plus1));
                             }
                             else if (processResult == StartReleaseResult.Started)
                             {
                                 // add rocket reaction to comment
-                                await _octokitClient.Reaction.IssueComment.Create(repositoryId, (int)payload.comment.id, new NewReaction(ReactionType.Rocket));
+                                await _octokitClient.Reaction.IssueComment.Create(repositoryId, (uint)payload.comment.id.Value, new NewReaction(ReactionType.Rocket));
                             }
                             else if (processResult == StartReleaseResult.WatchoutConditions)
                             {
                                 // add eyes reaction to comment
-                                await _octokitClient.Reaction.IssueComment.Create(repositoryId, (int)payload.comment.id, new NewReaction(ReactionType.Eyes));
+                                await _octokitClient.Reaction.IssueComment.Create(repositoryId, (uint)payload.comment.id.Value, new NewReaction(ReactionType.Eyes));
                             }
                             else
                             {
                                 // add confused reaction to comment
-                                await _octokitClient.Reaction.IssueComment.Create(repositoryId, (int)payload.comment.id, new NewReaction(ReactionType.Confused));
+                                await _octokitClient.Reaction.IssueComment.Create(repositoryId, (uint)payload.comment.id.Value, new NewReaction(ReactionType.Confused));
                             }
                         }
                         else
@@ -446,7 +446,7 @@ namespace nanoFramework.Tools.GitHub
                             log.LogInformation($"User has no permission to execute command");
 
                             // add thumbs down reaction to comment
-                            await _octokitClient.Reaction.IssueComment.Create(repositoryId, (int)payload.comment.id, new NewReaction(ReactionType.Minus1));
+                            await _octokitClient.Reaction.IssueComment.Create(repositoryId, (uint)payload.comment.id.Value, new NewReaction(ReactionType.Minus1));
                         }
                     }
                 }
@@ -998,7 +998,7 @@ namespace nanoFramework.Tools.GitHub
             else if (command.EndsWith("updatedependencies all"))
             {
                 // add thumbs up reaction to comment to flag start
-                await _octokitClient.Reaction.IssueComment.Create((long)payload.repository.id, (int)payload.comment.id, new NewReaction(ReactionType.Plus1));
+                await _octokitClient.Reaction.IssueComment.Create((long)payload.repository.id, (uint)payload.comment.id.Value, new NewReaction(ReactionType.Plus1));
 
                 return await UpdateDependenciesAllReposAsync(log);
             }
@@ -1037,7 +1037,7 @@ namespace nanoFramework.Tools.GitHub
             else if (command.EndsWith("closeupdateprs"))
             {
                 // add thumbs up reaction to comment to flag start
-                await _octokitClient.Reaction.IssueComment.Create((long)payload.repository.id, (int)payload.comment.id, new NewReaction(ReactionType.Plus1));
+                await _octokitClient.Reaction.IssueComment.Create((long)payload.repository.id, (uint)payload.comment.id.Value, new NewReaction(ReactionType.Plus1));
 
                 return await CloseUpdatePrsAsync(log);
             }
